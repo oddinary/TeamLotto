ab566a66093f66b6524d4c0e25fadd45a4e4c1c3
현진 <Administrator@SC-202106161522>
2022-06-29 11:45:04 +0900

    저도해봄 12341234

--- 220629-02 hello git2/src/Main.java
+++ 220629-02 hello git2/src/Main.java
@@ -9,6 +9,7 @@ public class Main {
 		System.out.println("앨랠레");
 		// 1. 최대한 의견 반영 하겠음
 		// 2. 안되는 건 안되는 거임
+		System.out.println("12341234");
 		
 		System.out.println("아무거나 쓰는중임ㅋ");
 		System.out.println("나도 해봄");
