9a926faafc50a98baddac4f9fab67ea4986c1628
성훈 <Administrator@SC-202106161522>
2022-06-29 12:11:36 +0900

    성훈 로또최종장

---
+++ BlackLotto/src/LottoEndPage.java
@@ -0,0 +1,54 @@
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+
+public class LottoEndPage extends JFrame {
+	public LottoEndPage() {
+		JPanel pnlLotto = new JPanel(new BorderLayout(0, 30));
+		JPanel pnlNorth = new JPanel(); // 회차
+		JLabel center = new JLabel("당첨번호");
+		JPanel pnlCenter = new JPanel(); // 당첨번호
+		JPanel pnlCenter2 = new JPanel(); // 당점결과
+		JLabel center2 = new JLabel();
+		JPanel pnlSouth = new JPanel(); // 로또번호
+
+		center.setFont(new Font("PLAIN", Font.PLAIN, 25));
+		center2.setFont(new Font("PLAIN", Font.CENTER_BASELINE, 20));
+
+		pnlSouth.setLayout(new BoxLayout(pnlSouth, BoxLayout.Y_AXIS));
+		pnlSouth.setBorder(new LineBorder(Color.BLACK));
+
+		pnlLotto.add(pnlNorth, "North");
+		pnlLotto.add(pnlCenter, "Center");
+		pnlLotto.add(pnlCenter2, "Center");
+		pnlLotto.add(pnlSouth, "South");
+//		pnlLotto.setBackground(Color.white);
+
+//		.setOpaque(true); //Opaque값을 true로 미리 설정해 주어야 배경색이 적용된다.
+//		.setBackgroud(Color.pink);
+		//위쪽 센터 배경색 끄기
+		pnlNorth.setOpaque(false);
+		pnlCenter.setOpaque(false);
+		setContentPane(pnlLotto);
+		pnlCenter.add(center);
+		
+		pack();
+		setSize(400, 700);
+		setVisible(true);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+//		addListeners();
+		
+		//창의 크기를 조절할수 없게끔
+		setResizable(false);
+	}
+
+	public static void main(String[] args) {
+		new LottoEndPage().setVisible(true);
+	}
+}
