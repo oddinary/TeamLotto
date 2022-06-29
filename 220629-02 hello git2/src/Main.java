1d53b7801f7264833b28354083630ceb18503364
jinho <Administrator@SC-202106161522>
2022-06-29 11:34:30 +0900

    수정하셈

--- 220629-02 hello git2/src/Main.java
+++ 220629-02 hello git2/src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("앨랠레");
+		System.out.println("팀장의 권한");
 		// 1. 최대한 의견 반영 하겠음
 		// 2. 안되는 건 안되는 거임
 		
+		System.out.println("맘에 안드는거 있으면 수정하셈");
 		System.out.println("아무거나 쓰는중임ㅋ");
 		System.out.println("나도 해봄");
 	}
