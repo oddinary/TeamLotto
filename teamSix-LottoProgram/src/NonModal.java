01b6f0b0b8c5cd66bda8ce73db9de8833102fe1e
ss <Administrator@SC-202106161522>
2022-07-04 17:51:39 +0900

    추천 번호 완성, 뜨는 위치 변경

--- teamSix-LottoProgram/src/NonModal.java
+++ teamSix-LottoProgram/src/NonModal.java
@@ -1,12 +1,19 @@
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
+import java.util.Set;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -16,11 +23,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
-import javax.swing.text.DateFormatter;
-import javax.swing.SwingConstants;
-import java.awt.Font;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
 
 public class NonModal extends JDialog {
 	NonModal(JFrame owner) {
@@ -28,18 +30,99 @@ public class NonModal extends JDialog {
 		setTitle("프리미엄 전용");
 
 		LocalDateTime d = LocalDateTime.now();
-		DateTimeFormatter  formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd, HH:mm");
-		
-		List<Integer> todayNumber = new LinkedList<>();
-		Random random = new Random();
-		while (todayNumber.size() < 6) {
-			int r = (random.nextInt(45)) + 1;
-			if (!todayNumber.contains(r)) {
-				todayNumber.add(r);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd, HH:mm");
+
+		// 누적 회차에 나온 수 중 많이 나온 순 리스트 저장 후, 가장 많이 나왔던 순서대로 오름차순 정렬후
+		// 추천번호에 입력.
+		////////////////////
+		Set<Integer> recNum = new HashSet<>();
+		List<Integer> recNumber = new ArrayList<>();
+		List<Integer> countNumber = new ArrayList<>();
+		for (int i = 0; i < Lotto.lottoFive.size(); i++) {
+			for (int j = 0; j < Lotto.lottoFive.get(i).size(); j++) {
+				recNum.add(Lotto.lottoFive.get(i).get(j));
 			}
+		}
+//		System.out.println(recNum);
+
+//		recNumber.addAll(recNum);
+//		int[] recommendArray = new int[recNum.size()];
+//		for (int i = 0; i < recNumber.size(); i++) {
+//			int number = recNumber.get(i);
+//			recommendArray[i] = number;
+//		}
+//		for (int i = 0; i < recommendArray.length; i++) {
+//			Set<Integer> num = new HashSet<>();
+//			num.add(recommendArray[i]);
+//		}
+
+		//////////////////////////////
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < Lotto.lottoFive.size(); i++) {
+			for (int j = 0; j < Lotto.lottoFive.get(i).size(); j++) {
+				int num = Lotto.lottoFive.get(i).get(j);
+				if (!map.containsKey(num)) {
+					map.put(num, 1);
+				} else {
+					map.put(num, map.get(num) + 1);
+				}
+			}
+		}
+
+		for (int i = 1; i <= 45; i++) {
+			if (map.containsKey(i)) {
+				if (map.get(i) >= 2) {
+
+					recNumber.add(i);
+					countNumber.add(map.get(i));
+
+				}
+			}
+		}
+
+//		System.out.println(map);
+
+		for (int i = 0; i < recNumber.size(); i++) {
+			for (int j = i + 1; j < recNumber.size(); j++) {
+				if (map.get(recNumber.get(i)) < map.get(recNumber.get(j))) {
+					int copy = recNumber.get(i);
+
+					recNumber.set(i, recNumber.get(j));
+					recNumber.set(j, copy);
+				}
+			}
+		}
+
+//		System.out.println(recNumber);
+
+		List<Integer> todayNumber = new ArrayList<>();
+		for (int i = 0; i < 6; i++) {
+			todayNumber.add(recNumber.get(i));
 		}
 		Collections.sort(todayNumber);
 //		System.out.println(todayNumber);
+
+//		for (int i = 0; i <= 45; i++) {
+//			if (!map.containsKey(i)) {
+//				map.put(i, 1);
+//			} else {
+//				map.put(i, map.get(i) + 1);
+//			}
+//		}
+//		
+		////////////
+
+//		Random random = new Random();
+//		List<Integer> todayNumber = new LinkedList<>();
+//		while (todayNumber.size() < 6) {
+//			int r = (random.nextInt(45)) + 1;
+//			if (!todayNumber.contains(r)) {
+//				todayNumber.add(r);
+//			}
+//		}
+//		Collections.sort(todayNumber);
+////		System.out.println(todayNumber);
 
 		TitledBorder tbNonModal = new TitledBorder(new LineBorder(Color.black), "추천 번호");
 		tbNonModal.setTitleColor(new Color(245, 136, 110));
@@ -59,7 +142,7 @@ public class NonModal extends JDialog {
 		panel.add(lblToday);
 		panel.add(lblNum);
 		pnl.add(pnlBtn);
-		
+
 		pnlBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -71,7 +154,7 @@ public class NonModal extends JDialog {
 		pack();
 //		setSize(230,120);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		setLocationRelativeTo(null);
+		setLocation(500, 500);
 
 	}
 }
