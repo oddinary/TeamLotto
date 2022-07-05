2208804af78fa14a9fea931aab5450e00c815ea3
진호 <Administrator@SC-202106161522>
2022-07-05 10:23:10 +0900

    입력창 ui 수정

--- teamSix-LottoProgram/src/MyInfo.java
+++ teamSix-LottoProgram/src/MyInfo.java
@@ -1,6 +1,7 @@
 
 import java.awt.Color;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
@@ -8,6 +9,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 import java.awt.GridLayout;
@@ -20,12 +22,24 @@ class NumInput extends JDialog {
 	public NumInput(JDialog owner) {
 		super(owner, true);
 		setTitle("입력 창");
-
-		tf = new JTextField(5);
-		add(tf, "North");
+		
+		JPanel pnlInput = new JPanel();
+		JPanel pnlbtn = new JPanel();
+		JLabel lblInput = new JLabel("금액 입력");
+		lblInput.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		pnlInput.setLayout(new GridLayout(3,0,0,5));
+		
+		tf = new JTextField(10);
 
 		JButton btn = new JButton("확인");
-		add(btn);
+		
+		pnlbtn.add(btn);
+		pnlInput.add(lblInput);
+		pnlInput.add(tf);
+		pnlInput.add(pnlbtn);
+		
+		add(pnlInput);
 
 		btn.addActionListener(new ActionListener() {
 			@Override
@@ -34,7 +48,9 @@ class NumInput extends JDialog {
 			}
 		});
 
-		setSize(150, 150);
+		pack();
+//		setSize(150,150);
+		setLocationRelativeTo(null);
 	}
 
 	public int showDialog() {
@@ -60,17 +76,53 @@ public class MyInfo extends JDialog {
 		JLabel lblName = new JLabel(user.getName() + " 님의 정보");
 		JPanel pnlbtn = new JPanel();
 
-		JPanel pnlMoney = new JPanel();
-
 		pnl.setBorder(tbMyInfo);
 
 		JPanel panel = new JPanel();
 		pnl.add(panel);
 		panel.setLayout(new GridLayout(6, 0, 0, 10));
-		panel.add(lblName);
+
 		JLabel lblPhone = new JLabel("전화번호 : " + user.getPhoneNum());
 		JLabel lblPrime = new JLabel("일반회원");
 		JLabel lblMoney = new JLabel("보유금 : " + user.getHaveMoney() + " 원");
+
+		JPanel pnlMoney = new JPanel();
+		JButton btnMoney = new JButton("입금");
+		btnMoney.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				NumInput dialog = new NumInput(MyInfo.this);
+				int result = dialog.showDialog();
+
+				if (result != 0) {
+					user.setHaveMoney(user.getHaveMoney() + result);
+					lblMoney.setText("보유금 : " + user.getHaveMoney() + " 원");
+					JOptionPane.showMessageDialog(MyInfo.this, "입금 완료되었습니다.");
+				}
+			}
+		});
+		pnlMoney.add(btnMoney);
+
+		// 환전 버튼
+		JButton btnChange = new JButton("출금");
+		btnChange.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				NumInput dialog = new NumInput(MyInfo.this);
+				int result = dialog.showDialog();
+
+				if (user.getHaveMoney() < result) {
+					JOptionPane.showMessageDialog(MyInfo.this, "보유금이 부족합니다.");
+				} else if (result == 0) {
+
+				} else {
+					user.setHaveMoney(user.getHaveMoney() - result);
+					lblMoney.setText("보유금 : " + user.getHaveMoney() + " 원");
+					JOptionPane.showMessageDialog(MyInfo.this, "출금 완료되었습니다.");
+				}
+			}
+		});
+		pnlMoney.add(btnChange);
+		panel.add(pnlMoney);
+		panel.add(lblName);
 
 		JButton btnPrime = new JButton("프리미엄 결제 (10,000)");
 		if (user.isPremier() == true) {
@@ -109,41 +161,6 @@ public class MyInfo extends JDialog {
 		panel.add(lblPrime);
 
 		panel.add(lblMoney);
-		JButton btnMoney = new JButton("입금");
-		btnMoney.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				NumInput dialog = new NumInput(MyInfo.this);
-				int result = dialog.showDialog();
-
-				if (result != 0) {
-					user.setHaveMoney(user.getHaveMoney() + result);
-					lblMoney.setText("보유금 : " + user.getHaveMoney() + " 원");
-					JOptionPane.showMessageDialog(MyInfo.this, "입금 완료되었습니다.");
-				}
-			}
-		});
-		pnlMoney.add(btnMoney);
-
-		// 환전 버튼
-		JButton btnChange = new JButton("출금");
-		btnChange.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				NumInput dialog = new NumInput(MyInfo.this);
-				int result = dialog.showDialog();
-
-				if (user.getHaveMoney() < result) {
-					JOptionPane.showMessageDialog(MyInfo.this, "보유금이 부족합니다.");
-				} else if (result == 0) {
-
-				} else {
-					user.setHaveMoney(user.getHaveMoney() - result);
-					lblMoney.setText("보유금 : " + user.getHaveMoney() + " 원");
-					JOptionPane.showMessageDialog(MyInfo.this, "출금 완료되었습니다.");
-				}
-			}
-		});
-		pnlMoney.add(btnChange);
-		panel.add(pnlMoney);
 		panel.add(pnlbtn);
 
 		getContentPane().add(pnl);
